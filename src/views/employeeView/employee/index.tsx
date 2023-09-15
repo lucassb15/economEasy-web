8982fccdefe78ad8a9c79c2cc260edb86bbb793f
Lucas Barbosa <soares_lucas13@hotmail.com>
2023-09-15 09:34:00 -0300

    🔥 Pagina NotFound

--- src/views/employeeView/employee/index.tsx
+++ src/views/employeeView/employee/index.tsx
@@ -1,28 +1,10 @@
-import { AuthContext } from '@contexts/AuthContext'
 import { Header } from '../../../components/Header'
-import { useContext } from 'react'
 import QRReader from '@components/QrCodeReader'
 export function Employee() {
-  const { user } = useContext(AuthContext)
-
   return (
     <div>
       <Header title={'Funcionário'} />
-      <h1>Tela inicial funcionário</h1>
-      <label htmlFor="">Role: </label>
-      <p>{user?.role}</p>
-      <label htmlFor="">Email: </label>
-      <p>{user?.email}</p>
-      <label htmlFor="">Id: </label>
-      <p>{user?.id}</p>
-      <label htmlFor="">CompanyId: </label>
-      <p>{user?.companyId}</p>
       <QRReader />
-      <p>.</p>
-      <p>.</p>
-      <p>.</p>
-      <p>.</p>
-      <p>.</p>
     </div>
   )
 }
